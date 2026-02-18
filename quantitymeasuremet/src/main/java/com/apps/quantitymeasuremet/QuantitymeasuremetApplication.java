349b940437c475dfd5f99db148ba4f997f04ba56
Prakash Dixit <prakashdixit879@gmail.com>
2026-02-18 16:28:57 +0530

    [Prakash Dixit] Add. Solution to uc2 problem statement.

--- quantitymeasuremet/src/main/java/com/apps/quantitymeasuremet/QuantitymeasuremetApplication.java
+++ quantitymeasuremet/src/main/java/com/apps/quantitymeasuremet/QuantitymeasuremetApplication.java
@@ -11,7 +11,13 @@ public class QuantitymeasuremetApplication {
 		private final double measurement;
 		
 		public Feet(double measurement) {
-			this.measurement= measurement;		
+			validateInput(measurement);
+			this.measurement= measurement;
+		}
+		
+		private void validateInput(double measurement) {
+		    if (Double.isNaN(measurement) || Double.isInfinite(measurement) || measurement<0)
+		        throw new IllegalArgumentException("Invalid value");
 		}
 		
 		@Override
@@ -34,13 +40,58 @@ public class QuantitymeasuremetApplication {
 		}
 		
 	}
+	
+	public static class Inches{
+		private final double measurement;
+		
+		public Inches(double measurement) {
+			validateInput(measurement);
+			this.measurement= measurement;
+		}
+		
+		private void validateInput(double measurement) {
+		    if (Double.isNaN(measurement) || Double.isInfinite(measurement) || measurement<0)
+		        throw new IllegalArgumentException("Invalid value");
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(this==obj) {
+				return true;
+			}
+			if(obj==null || this.getClass()!=obj.getClass()) {
+				return false;
+			}
+			
+			Inches object= (Inches) obj;
+			
+			return Double.compare(this.measurement, object.measurement)==0;
+		}
+		
+		@Override
+		public int hashCode() {
+			return Double.hashCode(measurement);
+		}
+	}
+	
+    public static boolean checkFeetEquality(double m1, double m2) {
+        Feet f1= new Feet(m1);
+        Feet f2= new Feet(m2);
+        return f1.equals(f2);
+    }
+
+    public static boolean checkInchesEquality(double m1, double m2) {
+        Inches i1= new Inches(m1);
+        Inches i2= new Inches(m2);
+        return i1.equals(i2);
+    }
+
 
 	public static void main(String[] args) {
 //		SpringApplication.run(QuantitymeasuremetApplication.class, args);
 			
-		Feet f1= new Feet(20);
-		Feet f2= new Feet(20);
-		System.out.println("Is F1 equals to F2?: "+ f1.equals(f2));
+		System.out.println("Feet Equal? " + checkFeetEquality(20, 20));
+        System.out.println("Inches Equal? " + checkInchesEquality(12, 12));
 		
 	}
 
--- quantitymeasuremet/src/test/java/com/apps/quantitymeasuremet/QuantitymeasuremetApplicationTests.java
+++ quantitymeasuremet/src/test/java/com/apps/quantitymeasuremet/QuantitymeasuremetApplicationTests.java
@@ -6,12 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.apps.quantitymeasuremet.QuantitymeasuremetApplication.Feet;
+import com.apps.quantitymeasuremet.QuantitymeasuremetApplication.Inches;
+
 
 @SpringBootTest
 class QuantitymeasuremetApplicationTests {
 
 	@Test
-	void testEquality_SameValue() {
+	void testEquality_SameValue_F() {
 		Feet f1= new Feet(1.0);
 		Feet f2= new Feet(1.0);
 		
@@ -19,7 +21,7 @@ class QuantitymeasuremetApplicationTests {
 	}
 	
 	@Test
-	void testEquality_DifferentValue() {
+	void testEquality_DifferentValue_F() {
 		Feet f1= new Feet(1.0);
 		Feet f2= new Feet(2.0);
 		
@@ -27,7 +29,7 @@ class QuantitymeasuremetApplicationTests {
 	}
 	
 	@Test
-	void testEquality_NullComparision() {
+	void testEquality_NullComparision_F() {
 		Feet f1= new Feet(20);
 		Feet f2= null;
 		
@@ -35,17 +37,55 @@ class QuantitymeasuremetApplicationTests {
 	}
 	
 	@Test
-	void testEquality_NonNumericalInput() {
+	void testEquality_NonNumericalInput_F() {
 		Feet f1= new Feet(20);
 		
 		assertEquals(false, f1.equals("l"));
 	}
 	
 	@Test
-	void testEquality_SameReference() {
+	void testEquality_SameReference_F() {
 		Feet f1= new Feet(10);
 		
 		assertEquals(true, f1.equals(f1));
+	}
+	
+	@Test
+	void testEquality_SameValue_I() {
+		Inches i1= new Inches(10);
+		Inches i2= new Inches(10);
+		
+		assertEquals(true, i1.equals(i2));
+	}
+	
+	@Test
+	void testEquality_DifferentValue_I() {
+		Inches i1= new Inches(1.0);
+		Inches i2= new Inches(2.0);
+		
+		assertEquals(false, i1.equals(i2));
+	}
+	
+	@Test
+	void testEquality_NullComparision_I() {
+		Inches i1= new Inches(20);
+		Inches i2= null;
+		
+		assertEquals(false, i1.equals(i2));
+	}
+	
+	@Test
+	void testEquality_NonNumericalInput_I() {
+		Inches i1= new Inches(20);
+		
+		assertEquals(false, i1.equals("l"));
+	}
+	
+	@Test
+	void testEquality_SameReference_I() {
+		Inches i1= new Inches(10);
+		
+		assertEquals(true, i1.equals(i1));
 	}
 
 }
