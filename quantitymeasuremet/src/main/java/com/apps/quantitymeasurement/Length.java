385fe26fc3d963775fa244ed271bf451f1c62623
Prakash Dixit <prakashdixit879@gmail.com>
2026-02-19 12:43:53 +0530

    [Prakash Dixit] Add. Solution to uc3 problem statement.

---
+++ quantitymeasuremet/src/main/java/com/apps/quantitymeasurement/Length.java
@@ -0,0 +1,73 @@
+package com.apps.quantitymeasurement;
+
+import java.util.Objects;
+
+public class Length {
+	
+	private final double value;
+	private final LengthUnit unit;
+	
+	public enum LengthUnit{
+		FEET(12.0),
+		INCHES(1.0);
+		
+		private final double conversionFactor;
+		
+		LengthUnit(double conversionFactor){
+			this.conversionFactor= conversionFactor;
+		}
+		
+		public double getConversion() {
+			return conversionFactor;
+		}
+		
+	}
+	
+	public Length(double value, LengthUnit unit) {
+		if(value<0) {
+			throw new IllegalArgumentException("Length cannot be negative");
+		}
+		if(unit==null) {
+			throw new IllegalArgumentException("Unit cannot be null");
+		}
+		
+		this.value= value;
+		this.unit= unit;
+	}
+	
+	public double convertToBaseUnit() {
+		return this.value*this.unit.conversionFactor;
+	}
+	
+	public boolean compare(Length thatLength) {
+		return Double.compare(this.convertToBaseUnit(), thatLength.convertToBaseUnit())==0;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this==o) return true;
+		
+		if(o==null || this.getClass()!=o.getClass()) return false;
+		
+		Length obj=(Length) o;
+		
+		return this.compare(obj);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Double.hashCode(this.convertToBaseUnit());
+	}
+	
+	@Override
+	public String toString() {
+		return value+":"+unit;
+	}
+	
+	public static void main(String[] args) {
+		Length l1= new Length(1.0, LengthUnit.FEET);
+		Length l2= new Length(12.0, LengthUnit.INCHES);
+		System.out.println("Equal? "+l1.equals(l2));
+	}
+	
+}
--- quantitymeasuremet/src/test/java/com/apps/quantitymeasuremet/QuantitymeasuremetApplicationTests.java
+++ quantitymeasuremet/src/test/java/com/apps/quantitymeasuremet/QuantitymeasuremetApplicationTests.java
@@ -1,91 +1,83 @@
 package com.apps.quantitymeasuremet;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import com.apps.quantitymeasuremet.QuantitymeasuremetApplication.Feet;
-import com.apps.quantitymeasuremet.QuantitymeasuremetApplication.Inches;
+import com.apps.quantitymeasurement.Length;
 
-
-@SpringBootTest
 class QuantitymeasuremetApplicationTests {
 
 	@Test
-	void testEquality_SameValue_F() {
-		Feet f1= new Feet(1.0);
-		Feet f2= new Feet(1.0);
+	void testEquality_FeetToFeet_SameValue() {
 		
-		assertEquals(true, f1.equals(f2));
+		assertEquals(true, new Length(1.0, Length.LengthUnit.FEET).equals(new Length(1.0, Length.LengthUnit.FEET)));
+		
 	}
 	
 	@Test
-	void testEquality_DifferentValue_F() {
-		Feet f1= new Feet(1.0);
-		Feet f2= new Feet(2.0);
+	void testEquality_InchToInch_SameValue() {
 		
-		assertEquals(false, f1.equals(f2));
+		assertEquals(true, new Length(1.0, Length.LengthUnit.INCHES).equals(new Length(1.0, Length.LengthUnit.INCHES)));
+		
 	}
 	
 	@Test
-	void testEquality_NullComparision_F() {
-		Feet f1= new Feet(20);
-		Feet f2= null;
+	void testEquality_FeetToInch_SameValue() {
 		
-		assertEquals(false, f1.equals(f2));
+		assertEquals(true, new Length(1.0, Length.LengthUnit.FEET).equals(new Length(12.0, Length.LengthUnit.INCHES)));
+		
 	}
 	
 	@Test
-	void testEquality_NonNumericalInput_F() {
-		Feet f1= new Feet(20);
+	void testEquality_InchToFeet_SameValue() {
 		
-		assertEquals(false, f1.equals("l"));
+		assertEquals(true, new Length(12.0, Length.LengthUnit.INCHES).equals(new Length(1.0, Length.LengthUnit.FEET)));
+		
 	}
 	
 	@Test
-	void testEquality_SameReference_F() {
-		Feet f1= new Feet(10);
+	void testEquality_FeetToFeet_DifferentValue() {
 		
-		assertEquals(true, f1.equals(f1));
+		assertEquals(false, new Length(1.0, Length.LengthUnit.FEET).equals(new Length(2.0, Length.LengthUnit.FEET)));
+		
 	}
 	
 	@Test
-	void testEquality_SameValue_I() {
-		Inches i1= new Inches(10);
-		Inches i2= new Inches(10);
+	void testEquality_InchToInch_DifferentValue() {
 		
-		assertEquals(true, i1.equals(i2));
+		assertEquals(true, new Length(1.0, Length.LengthUnit.FEET).equals(new Length(1.0, Length.LengthUnit.FEET)));
+		
 	}
 	
 	@Test
-	void testEquality_DifferentValue_I() {
-		Inches i1= new Inches(1.0);
-		Inches i2= new Inches(2.0);
+	void testEquality_InvalidUnit() {
 		
-		assertEquals(false, i1.equals(i2));
+		assertThrows(IllegalArgumentException.class, ()->new Length(1.0, null));
+		
 	}
 	
 	@Test
-	void testEquality_NullComparision_I() {
-		Inches i1= new Inches(20);
-		Inches i2= null;
-		
-		assertEquals(false, i1.equals(i2));
-	}
-	
-	@Test
-	void testEquality_NonNumericalInput_I() {
-		Inches i1= new Inches(20);
-		
-		assertEquals(false, i1.equals("l"));
-	}
-	
-	@Test
-	void testEquality_SameReference_I() {
-		Inches i1= new Inches(10);
-		
-		assertEquals(true, i1.equals(i1));
-	}
+    void testEquality_NullUnit() {
 
+        assertThrows(IllegalArgumentException.class, ()->new Length(5.0, null));
+        
+    }
+	
+	@Test
+    void testEquality_SameReference() {
+		
+		assertTrue(new Length(10, Length.LengthUnit.INCHES).equals(new Length(10, Length.LengthUnit.INCHES)));
+		
+	}
+	
+	@Test
+	void testEquality_NullComparision() {
+		
+		assertFalse(new Length(10, Length.LengthUnit.INCHES).equals(null));
+		
+	}
 }
